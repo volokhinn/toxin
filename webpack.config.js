6456cb545702859f0cdb068366cbeab4531fe2d4
volokhinn <csgomx@mail.ru>
2022-03-20 14:35:54 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 4200,
+    },
+    entry: './index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            inject: 'body',
+            template: 'index.pug',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].bundle.css'
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /nodeModules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s[ac]ss|css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.pug$/,
+                use: [{
+                    loader: '@webdiscus/pug-loader',
+                    options: {
+                        pretty: true
+                    }
+                }, ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[hash][ext][query]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[hash][ext][query]'
+                },
+            },
+        ]
+    }
+};
